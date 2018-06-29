580b4e96500590f4509902fdb5fe26840e26dfa1
MacBook <mac@MacBooks-MacBook-Pro.local>
2018-06-30 00:56:54 +0100

    init

--- src/containers/__tests__/JobTabSwitcher/listItems.test.js
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import ListItem from '../../JobsTabSwitcher/listItem'
-
-it('renders without crashing', () => {
-	expect(shallow(<ListItem/>)).toMatchSnapshot()
-})
--- src/containers/__tests__/MainFooter/index.test.js
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import { MainFooter } from '../../MainFooter/'
-
-it('renders without crashing', () => {
-	expect(shallow(<MainFooter />)).toMatchSnapshot()
-})
